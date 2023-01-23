b290f01fe51151ad4c0a1e584c489399507f114f
Sukanta Das <arnobh84@gmail.com>
2023-01-23 18:47:43 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-4xl">SERVER RUNNING </p>
+    </div>
+  );
+}
+
+export default App;
